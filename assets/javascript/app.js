f2e50731b153ff8703089294b432349c3bff64fa
J91Branson <jbcod4@gmail.com>
2019-03-16 19:25:16 -0500

    will show current time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,65 +1,70 @@
-var config = {
+
     
 
-    // Initialize Firebase
+    // Initialize Firebase   
     
-      apiKey: "AIzaSyDWC2YRCkUZtkjmRrUo_rAmOpUbk-jYVIA",
-      authDomain: "train-scheduler-f0265.firebaseapp.com",
-      databaseURL: "https://train-scheduler-f0265.firebaseio.com",
-      projectId: "train-scheduler-f0265",
-      storageBucket: "train-scheduler-f0265.appspot.com",
-      messagingSenderId: "346189852819"
-   
+    var config = {
+        apiKey: "AIzaSyDWC2YRCkUZtkjmRrUo_rAmOpUbk-jYVIA",
+        authDomain: "train-scheduler-f0265.firebaseapp.com",
+        databaseURL: "https://train-scheduler-f0265.firebaseio.com",
+        projectId: "train-scheduler-f0265",
+        storageBucket: "train-scheduler-f0265.appspot.com",
+        messagingSenderId: "346189852819"
+    };
     
-  
-  };
-  firebase.initializeApp(config);
-  
-   var database = firebase.database();
-   var employeeList = database.ref('/employee');
-  
-   employeeList.on('child_added', function(snap){
-  
-         var formattedDate = moment.unix(record.start).format('MM/DD/YYYY');
-  
-       $('tbody').append(`
-           <tr>
-               <td>${snap.val().name}</td>
-               <td>${snap.val().role}</td>
-               <td>${snap.val().email}</td>
-               <td>${snap.val().rate}</td>
-               <td>${snap.val().startDate}</td>
-               <td>${snap.val()}
-           </tr>
-         `)
-   })
-  
-   $("#submitButton").on("click", function() {
+    
+    
+    firebase.initializeApp(config);
+    
+    var database = firebase.database();
+    var employeeList = database.ref('/employee');
+    var name = "";
+    var destination = "";
+    var frequency = "";
+    var firstTrain = "";
+    var currentTime = moment().format('HH:MM');  
+    employeeList.on('child_added', function(snap){
+        
+        //  var formattedDate = moment.unix(record.start).format('MM/DD/YYYY');
+        
+        $('tbody').append(`
+        <tr>
+        <td>${snap.val().name}</td>
+        <td>${snap.val().destination}</td>
+        <td>${snap.val().frequency}</td>
+        <td>${snap.val().firstTrain}</td>
+        <td>${snap.val()}
+        </tr>
+        `)
+    })
+    
+    $("#submitButton").on("click", function() {
      event.preventDefault();
+     console.log(currentTime);
+
+     
   
        var newRow = $("<div>");
        newRow.addClass("doesThisWork");
        newRow.append("<div class='row'>");
   
-       var name = "";
-       var role = "";
-       var email = "";
-       var rate = "";
-       var startDate = "";
+       
   
   
-       name = $("#employeeName").val().trim();
-       role = $("#employeeRole").val().trim();
-       email = $("#employeeEmail").val().trim();
-       rate = $("#employeeRate").val().trim();
-       startDate = $("#employeeStart").val().trim();
+       name = $("#trainName").val().trim();
+       destination = $("#destination").val().trim();
+       frequency = $("#frequency").val().trim();
+       firstTrain = $("#firstTrain").val().trim();
+       
   
        database.ref("/employee").push({
          name: name,
-         role: role,
-         email: email,
-         rate: rate,
-         startDate: startDate
+         destination: destination,
+         frequency: frequency,
+         firstTrain: firstTrain,
+         
   
        })
      });
+
+     
